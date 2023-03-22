08e3d6dfba1fb59bfedaf918c39aab729920376d
BILLKOLOS <billooko2@gmail.com>
2023-03-22 02:52:41 -0700

    admin update

---
+++ admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>iPhone Inventory System - Admin Login</title>
+</head>
+<body>
+	<h1>iPhone Inventory System - Admin Login</h1>
+	<form action="admin.php" method="POST">
+		<label for="username">Username:</label><br>
+		<input type="text" id="username" name="username"><br>
+		<label for="password">Password:</label><br>
+		<input type="password" id="password" name="password"><br>
+		<input type="submit" value="Login">
+	</form>
+</body>
+</html>
+
