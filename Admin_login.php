8bd40bce40490e3f417d1e398eebfa47110b37f2
BILLKOLOS <billooko2@gmail.com>
2023-03-22 02:03:30 -0700

    admin authentication

---
+++ Admin_login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>iPhone Inventory System - Admin Login</title>
+</head>
+<body>
+	<h1>iPhone Inventory System - Admin Login</h1>
+	<form action="admin.php" method="POST">
+		<label for="username">Username:</label><br>
+		<input type="text" id="username" name="username"><br>
+		<label for="password">
+
